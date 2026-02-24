2cfee3984175570a9dec003fe59a96f444f5a683
Oleksandr Cherevach <oleksandr_cherevach@epam.com>
2026-02-24 17:12:27 +0200

    chore(eslint): allow codegen deep imports in specs

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,12 @@
 module.exports = {
   root: true,
   extends: '@react-native',
+  overrides: [
+    {
+      files: ['src/specs/**/*.ts', 'src/specs/**/*.tsx'],
+      rules: {
+        '@react-native/no-deep-imports': 'off',
+      },
+    },
+  ],
 };
